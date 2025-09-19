0cfad9bf4e2c5f0bd3b363ded663d469fe6977b2
ZihaoFU245 <ZihaoFU245@users.noreply.github.com>
2025-09-19 04:34:26 +0000

    deploy: 6ef864935c69a318e74fbb4572d321afc1845129

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ def string_to_secret(text: str) -> str:
     digest = hashlib.sha256(data).digest()
     return base64.urlsafe_b64encode(digest).rstrip(b"=").decode()
 
-print(string_to_secret("A phantom dance of keys, a hidden screen's glow, Beneath the drone of lectures, a private world I know."))
+print(string_to_secret(""))
 print(string_to_secret(""))
 # You are smart if you descover this
 # But no!!! 
